a50adb3010c42901012d4210871abcdbc375d186
Jidapa Angsutti <jidapa.angsutti@students.snow.edu>
2025-04-24 18:15:50 -0600

    demo done

---
+++ Maps.cs
@@ -0,0 +1,252 @@
+public class Maps
+{
+    public static List<string[]> Home(int movementTime)
+    {
+        // First move = player at 39th index of the line
+        int playerColumn = 39 + movementTime;
+        int playerRow = 11;
+
+        List<char[]> toDisplay = new();
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("..........................／l、.......................".ToCharArray());
+        toDisplay.Add("........................（ﾟ､ ｡７......................".ToCharArray());
+        toDisplay.Add("..........................l、ﾞ~ヽ.....................".ToCharArray());
+        toDisplay.Add("..........................じしf_, )ノ................".ToCharArray());
+        toDisplay.Add("..........................◢■■■■■◣.....................".ToCharArray());
+        toDisplay.Add("..............................*.......................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+
+
+        List<string[]> lines = new();
+        for (int i = 0; i < toDisplay.Count; i++)
+        {
+            char[] line = toDisplay[i];
+            string[] stringArray = new string[line.Length];
+
+            for (int j = 0; j < line.Length; j++)
+            {
+                if (i == playerRow && j == playerColumn)
+                {
+                    stringArray[j] = "O";
+                }
+                else if (line[j] == '.')
+                {
+                    stringArray[j] = " ".ToString();
+                }
+                else
+                {
+                    stringArray[j] = line[j].ToString();
+                }
+            }
+
+            lines.Add(stringArray);
+        }
+
+        return lines;
+    }
+
+        public static List<string[]> HomeBackward(int movementTime)
+    {
+        // First move = player at 39th index of the line
+        int playerColumn = 50 - movementTime;
+        int playerRow = 11;
+
+        List<char[]> toDisplay = new();
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("..........................／l、.......................".ToCharArray());
+        toDisplay.Add("........................（ﾟ､ ｡７......................".ToCharArray());
+        toDisplay.Add("..........................l、ﾞ~ヽ.....................".ToCharArray());
+        toDisplay.Add("..........................じしf_, )ノ.................".ToCharArray());
+        toDisplay.Add("..........................◢■■■■■◣.....................".ToCharArray());
+        toDisplay.Add("..............................*.......................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+
+
+        List<string[]> lines = new();
+        for (int i = 0; i < toDisplay.Count; i++)
+        {
+            char[] line = toDisplay[i];
+            string[] stringArray = new string[line.Length];
+
+            for (int j = 0; j < line.Length; j++)
+            {
+                if (i == playerRow && j == playerColumn)
+                {
+                    stringArray[j] = "O";
+                }
+                else if (line[j] == '.')
+                {
+                    stringArray[j] = " ".ToString();
+                }
+                else
+                {
+                    stringArray[j] = line[j].ToString();
+                }
+            }
+
+            lines.Add(stringArray);
+        }
+
+        return lines;
+    }
+
+    public static List<string[]> RanchForward(int movementTime)
+    {
+        // First move = player just arrived at the map Row 11 Column 0
+        int playerColumn = 0 + movementTime;
+        int playerRow = 11;
+
+        List<char[]> toDisplay = new();
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("..................................................V...".ToCharArray());
+        toDisplay.Add("..................M...................................".ToCharArray());
+        toDisplay.Add("............V.........................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add(".......................E..............................".ToCharArray());
+        toDisplay.Add("..................................................E...".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("...........................................V..........".ToCharArray());
+        toDisplay.Add(".........EVMV.........................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("............E.........................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add(".......................V..............................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......V...............................................".ToCharArray());
+        toDisplay.Add("..................................................V...".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+
+
+        List<string[]> lines = new();
+
+        for (int i = 0; i < toDisplay.Count; i++)
+        {
+            char[] line = toDisplay[i];
+            string[] stringArray = new string[line.Length];
+
+            for (int j = 0; j < line.Length; j++)
+            {
+                if (i == playerRow && j == playerColumn)
+                {
+                    stringArray[j] = "O";
+                }
+                else if (line[j] == '.')
+                {
+                    stringArray[j] = " ".ToString();
+                }
+                else
+                {
+                    stringArray[j] = line[j].ToString();
+                }
+            }
+
+            // fix collected items display
+            if (playerColumn >= 9)
+            {
+                int collectedItems = playerColumn - 9;
+
+                for (int k = 1; k <= collectedItems; k++)
+                {
+                    stringArray[playerColumn - k] = " ";
+                }
+            }
+
+            lines.Add(stringArray);
+        }
+
+        return lines;
+    }
+
+        public static List<string[]> RanchBackward(int movementTime)
+    {
+        // First move = player just arrived at the map Row 11 Column 0
+        int playerColumn = 12 - movementTime;
+        int playerRow = 11;
+
+        List<char[]> toDisplay = new();
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("..................................................V...".ToCharArray());
+        toDisplay.Add("..................M...................................".ToCharArray());
+        toDisplay.Add("............V.........................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add(".......................E..............................".ToCharArray());
+        toDisplay.Add("..................................................E...".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("...........................................V..........".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("............E.........................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add(".......................V..............................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+        toDisplay.Add("......V...............................................".ToCharArray());
+        toDisplay.Add("..................................................V...".ToCharArray());
+        toDisplay.Add("......................................................".ToCharArray());
+
+
+        List<string[]> lines = new();
+
+        for (int i = 0; i < toDisplay.Count; i++)
+        {
+            char[] line = toDisplay[i];
+            string[] stringArray = new string[line.Length];
+
+            for (int j = 0; j < line.Length; j++)
+            {
+                if (i == playerRow && j == playerColumn)
+                {
+                    stringArray[j] = "O";
+                }
+                else if (line[j] == '.')
+                {
+                    stringArray[j] = " ".ToString();
+                }
+                else
+                {
+                    stringArray[j] = line[j].ToString();
+                }
+            }
+
+            lines.Add(stringArray);
+        }
+
+        return lines;
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,153 @@
+﻿// ......................................................
+// Task: Cook fried chicken (eggs, meat, flour) and feed Cat
+// Flour: vegetables & vegetables
+// Walking to get ingredients
+// for (int i = 0; i < 11; i++)
+// {
+//     Console.Clear();
+//     var lines = Maps.Home(i);
+
+//     Console.WriteLine("________________________________________________________");
+//     foreach (var line in lines)
+//     {
+//         Console.Write("|");
+//         Console.Write(string.Join("", line));
+//         Console.Write("|");
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine("________________________________________________________");
+
+//     Console.ReadKey(true);
+// }
+
+
+for (int i = 0; i < 13; i++)
+{
+    Console.Clear();
+    var lines = Maps.RanchForward(i);
+
+    Console.WriteLine("________________________________________________________");
+    foreach (var line in lines)
+    {
+        Console.Write("|");
+        Console.Write(string.Join("", line));
+        Console.Write("|");
+        Console.WriteLine();
+    }
+    Console.WriteLine("________________________________________________________");
+
+    Console.ReadKey(true);
+}
+
+for (int i = 1; i < 13; i++)
+{
+    Console.Clear();
+    var lines = Maps.RanchBackward(i);
+
+    Console.WriteLine("________________________________________________________");
+    foreach (var line in lines)
+    {
+        Console.Write("|");
+        Console.Write(string.Join("", line));
+        Console.Write("|");
+        Console.WriteLine();
+    }
+    Console.WriteLine("________________________________________________________");
+
+    Console.ReadKey(true);
+}
+
+for (int i = 0; i < 16; i++)
+{
+    Console.Clear();
+    var lines = Maps.HomeBackward(i);
+
+    Console.WriteLine("________________________________________________________");
+    for (int j = 0; j < lines.Count; j++)
+    {
+        string[] line = lines[j];
+        // last step; i = 15: Print "Press F to Cook" on like row 8
+        Console.Write("|");
+        Console.Write(string.Join("", line));
+        Console.Write("|");
+        if (i == 15 && j == 8)
+        {
+            Console.Write("     Press F to Cook");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine("________________________________________________________");
+
+    Console.ReadKey(true);
+}
+
+// Cooking Screen
+Console.Clear();
+Console.WriteLine("Cat's Request: Fried Chicken (egg, meat, flour)");
+Console.WriteLine("Inventory: Egg (1), Meat (1), Vegetable (2)");
+Console.WriteLine("What you'd like to make?");
+Console.WriteLine("1. Steak");
+Console.WriteLine("2. Salt");
+Console.WriteLine("3. Flour");
+Console.WriteLine("4. Sugar");
+Console.WriteLine("5. Fried Chicken");
+
+// You click 3
+Console.ReadLine();
+
+Console.WriteLine("\nFlour: vegetable + vegetable. Proceed? (Y/N)");
+// You enter Y
+Console.ReadLine();
+
+Console.WriteLine("+ Flour (1)");
+Console.WriteLine("- Vegetable (2)");
+Console.ReadKey();
+
+// Cooking screen again. This time make the dish!
+Console.Clear();
+Console.WriteLine("Cat's Request: Fried Chicken (egg, meat, flour)");
+Console.WriteLine("Inventory: Egg (1), Meat (1), Flour (1)");
+Console.WriteLine("What you'd like to make?");
+Console.WriteLine("1. Steak");
+Console.WriteLine("2. Salt");
+Console.WriteLine("3. Flour");
+Console.WriteLine("4. Sugar");
+Console.WriteLine("5. Fried Chicken");
+
+// You click 5
+Console.ReadLine();
+
+Console.WriteLine("Fried Chicken: egg + meat + flour. Proceed? (Y/N)");
+// You enter Y
+Console.ReadLine();
+
+Console.WriteLine("\n+ Fried Chicken (1)");
+Console.WriteLine("- Meat (1)");
+Console.WriteLine("- Flour (1)");
+Console.ReadKey();
+
+// Go back home. Press "E" to feed
+Console.Clear();
+var lines1 = Maps.HomeBackward(15);
+
+Console.WriteLine("________________________________________________________");
+for (int j = 0; j < lines1.Count; j++)
+{
+    string[] line = lines1[j];
+    // last step; i = 15: Print "Press F to Cook" on like row 8
+    Console.Write("|");
+    Console.Write(string.Join("", line));
+    Console.Write("|");
+    if (j == 8)
+    {
+        Console.Write("     Press E to Feed");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("________________________________________________________");
+
+Console.ReadKey(true);
+
+Console.Clear();
+Console.WriteLine("Cat liked your Fried Chicken!");
+Console.WriteLine("You made Cat happy (for today)");
